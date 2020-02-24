bc0e601f8a176a11aec3af1db3204f120df41388
Anne de Joly <anne.dejoly@edhec.com>
2020-02-24 17:04:39 +0000

    added seeds, 5 users with 2 items each

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating the users...'
+
+5.times do
+  attributes = {
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    password: Faker::Internet.password(min_length: 8, mix_case: true, special_characters: true),
+    location: Faker::Address.street_address,
+    email: Faker::Internet.email
+  }
+  user = User.create!(attributes)
+
+  2.times do
+    item_attr = {
+      name: Faker::House.furniture,
+      description: Faker::TvShows::TheFreshPrinceOfBelAir.quote,
+      category: Item::CATEGORIES.sample,
+      user: user
+    }
+    Item.create!(item_attr)
+  end
+end
+
+puts 'Users and items created!'
