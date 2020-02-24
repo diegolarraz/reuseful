626c74665ea585fb8c4506955f1c0f63b9a8834f
Rory O'Neill <rory_oneill_1989@hotmail.com>
2020-02-24 16:39:25 +0000

    item controller created with methods and index view created

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,53 @@
+class ItemsController < ApplicationController
+  before_action set_item, only: %w(show edit update destroy)
+  skip_before_action :authenticate_user!, only: %w(index show)
+
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    if @item.save
+      redirect_to item_path(@item)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to items_path
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category)
+  end
+end
