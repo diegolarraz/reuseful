c75136817557285f27326d11f2cda5643d510021
diegolarraz <diegolarraz1@gmail.com>
2020-02-25 12:35:02 +0000

    worked on exchanges controller added calendar to pass the datetime instance, added routes and changed the unecessary nested routes. Need to implement the destroy method on exchange once user profile page is up and changing the new exchange to item show page

---
+++ app/controllers/exchanges_controller.rb
@@ -0,0 +1,28 @@
+class ExchangesController < ApplicationController
+  def new
+    @item = Item.find(params[:item_id])
+    @exchange = Exchange.new
+  end
+
+  def create
+    @exchange = Exchange.new(exchange_params)
+    # we need `restaurant_id` to associate review with corresponding restaurant
+    @item = Item.find(params[:item_id])
+    @exchange.item = @item
+    @exchange.user = current_user
+    if @exchange.save
+      redirect_to item_path(@item)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def exchange_params
+    params.require(:exchange).permit(:location, :date)
+  end
+end
