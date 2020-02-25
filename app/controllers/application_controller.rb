143ce206a7f316962165eb06188554cd811408e7
diegolarraz <diegolarraz1@gmail.com>
2020-02-25 15:37:57 +0000

    redirect user to items page upon login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || items_path
+  end
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :location])
