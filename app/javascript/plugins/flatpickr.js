1024b0b20c78a6b803d23dac6ffa5c223e0740e9
diegolarraz <diegolarraz1@gmail.com>
2020-02-26 11:29:05 +0000

    changed fonts and made date work again

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,15 +2,12 @@ import flatpickr from "flatpickr"
 import "flatpickr/dist/themes/material_green.css" // A path to the theme CSS
 import Siema from 'siema';
 
-flatpickr(".datepicker", {
-  enableTime: true
-})
+const datepicker = document.querySelector(".datepicker")
 
-document.addEventListener('turbolinks:load', () => {
-  // Call your JS functions here
-  const mySiema = new Siema({
-    duration: 250,
-    loop: true,
-  });
-  setInterval(() => mySiema.next(), 5000)
-});
+if (datepicker) {
+  flatpickr(".datepicker", {
+    enableTime: true
+  })
+}
+
+
