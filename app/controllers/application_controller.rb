eb067d6fbcced3a3c6e3c727769cb535e05028fe
Anne de Joly <anne.dejoly@edhec.com>
2020-02-26 14:52:52 +0000

    profile picture addeeeed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :location])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :location, :photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:email])
