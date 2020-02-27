a0e3cdd03e76ba2d1b6da79ca092ec782c59144f
diegolarraz <diegolarraz1@gmail.com>
2020-02-27 11:46:37 +0000

    trying

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,5 +1,4 @@
 import flatpickr from "flatpickr"
-import "flatpickr/dist/flatpickr.min.css"  // A path to the theme CSS
 
 document.addEventListener('turbolinks:load', () => {
   // Call your JS functions here
