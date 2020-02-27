b966d1dbd2a36634de5e9177e3cf07edcbdb3b4b
Alexandra Ionita <alexandraa_ionita@yahoo.com>
2020-02-27 15:14:30 +0000

    changes added to search bar

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get '/profile', to: 'pages#profile'
   devise_for :users
+  # shortcut for all the routes (show, edit, update, new, etc.)
   resources :items do
-    resources :comments, only: %w(new create)
-    resources :exchanges, only: %w(new create)
+    # shortcut for => get "/items/:item_id/comments/new"
+    resources :comments, only: [:new, :create]
+    resources :exchanges, only: [:new, :create]
   end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :comments, only: [:destroy]
   resources :exchanges, only: [:destroy]
