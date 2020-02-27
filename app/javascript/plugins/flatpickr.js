ef277e78fda1c5dcd5ba8a357515ca48da5ed2bc
diegolarraz <diegolarraz1@gmail.com>
2020-02-27 11:39:01 +0000

    trying

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,12 +1,16 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css"  // A path to the theme CSS
 
-const datepicker = document.querySelector(".datepicker")
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  const datepicker = document.querySelector(".datepicker")
 
-if (datepicker) {
-  flatpickr(".datepicker", {
-    enableTime: true
-  })
-}
+  if (datepicker) {
+    flatpickr(".datepicker", {
+      enableTime: true
+    })
+  }
 
+
+});
 
